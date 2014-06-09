f5784f2bb4b0ad68ad8e39bb9aa632653c35f249
Mirraine <mirraine.grech@mcast.edu.mt>
2014-06-10 01:07:53 +0200

    Commit9-Issue9-Enemies-Solved
    The game now has enemies so that the player can shoot at them to avoid
    losing his life and complete the levels.

---
+++ Issues Solved/Asteroid_Generator.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+var asteroid:Rigidbody;
+
+function createAsteroid()
+{
+	var xposition:float;
+	var yposition:float;
+	
+	xposition = Camera.main.transform.position.x+8;
+	yposition = -1;
+
+	Instantiate(asteroid,Vector3(xposition,yposition,-9.012),Quaternion.identity);
+}
+
+
+function Start () {
+
+	InvokeRepeating("createAsteroid",2.0,4.0);
+}
+
+function Update () {
+
+}
---
+++ Issues Solved/puddlegenerator.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+var puddle:Rigidbody;
+
+function createPuddle()
+{
+	var xposition:float;
+	var yposition:float;
+	
+	xposition = Camera.main.transform.position.x+6;
+	yposition = -4;
+	
+	Instantiate(puddle,Vector3(xposition,yposition,1),Quaternion.identity);
+}
+
+
+function Start () {
+
+	InvokeRepeating("createPuddle",2.0,4.0);
+}
+
+function Update () {
+
+}
